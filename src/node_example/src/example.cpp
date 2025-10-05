f8bf68df0cfe3f774392f27e1421eaaed25f4459
Shenxw <shenxw@stumail.neu.edu.cn>
2025-10-05 12:20:25 +0800

    1

--- src/node_example/src/example.cpp
+++ src/node_example/src/example.cpp
@@ -7,15 +7,7 @@ int main(int argc, char ** argv)
   (void) argv;
 
   cv::Mat image = cv::imread("image.jpg");
-  if (image.empty()) {
-    printf("Could not open or find the image\n");
-    return -1;
-  }
 
-  cv::imshow("Display window", image);
-  cv::waitKey(0);
-  // return 0;
-
-  printf("hello world hello_l package\n");
+  printf("Hello world!\n");
   return 0;
 }
